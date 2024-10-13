50a172a474a244e9b7a548e822d8bac956740171
Jose Valero Sanchis <valerosanchisjose@gmail.com>
2024-10-13 15:22:25 +0200

    Finishing app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
+import re
 import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import RobustScaler
 import joblib  
 
 # Título de la aplicación
-st.title("Predicción de Diagnóstico de Tumores Cerebrales")
+st.title("Prediction of Brain Tumor Diagnosis")
 
 st.write("""
-Esta aplicación permite cargar datos de concentraciones de metabolitos para predecir el tipo de tumor cerebral.
+This application allows uploading data of metabolite concentrations to predict brain tumor type.
 """)
 
 # Cargar el modelo guardado
@@ -28,17 +29,33 @@ model = load_saved_model()
 scaler = load_scaler()
 
 # Cargar el archivo CSV de entrada
-uploaded_file = st.file_uploader("Carga un archivo CSV con las concentraciones de metabolitos", type=["csv"])
+uploaded_file = st.file_uploader("Upload a CSV file with metabolite concentrations.", type=["csv"])
 
 if uploaded_file is not None:
     # Leer el archivo CSV
-    input_data = pd.read_csv(uploaded_file)
+    input_data = pd.read_csv(uploaded_file, sep = ";", usecols=range(1,16))
+
+    metabolite_columns = input_data.columns[input_data.columns != 'TYPE']
     
-    st.write("Datos cargados:")
+    def clean_number_format(value):
+        # Eliminar todas las comas y cualquier punto existente
+        cleaned_value = re.sub(r'[,.]', '', value)
+        
+        # Insertar un punto después del primer dígito
+        cleaned_value = f"{cleaned_value[0]}.{cleaned_value[1:]}"
+        
+        return cleaned_value
+
+    # Aplicar la función de limpieza y luego convertir a float
+    input_data[metabolite_columns] = input_data[metabolite_columns].apply(
+        lambda col: col.str.replace(',', '.').apply(clean_number_format).astype(float)
+    )    
+
+    st.write("Data loaded:")
     st.write(input_data.head())
 
     # Verificar si las columnas necesarias están presentes
-    required_columns = [f"Metabolite {i}" for i in range(15)]  # Lista de nombres de columnas de metabolitos esperados
+    required_columns = [f"METABOLITE {i}" for i in range(15)]  # Lista de nombres de columnas de metabolitos esperados
     if all(col in input_data.columns for col in required_columns):
         # Preprocesar los datos
         X_input = input_data[required_columns]
@@ -55,10 +72,10 @@ if uploaded_file is not None:
         # Crear un DataFrame con los resultados
         results = input_data.copy()
         for idx, diagnosis in enumerate(diagnosis_classes):
-            results[f'Probabilidad_{diagnosis}'] = predictions[:, idx]
-        results['Diagnóstico_Predicho'] = predicted_labels
+            results[f'Probability_{diagnosis}'] = predictions[:, idx]
+        results['Diagnosis_Prediction'] = predicted_labels
         
-        st.write("Resultados de la predicción:")
+        st.write("Prediction results:")
         st.write(results.head())
         
         # Botón para descargar los resultados
@@ -69,10 +86,10 @@ if uploaded_file is not None:
         csv = convert_df(results)
         
         st.download_button(
-            label="Descargar resultados como CSV",
+            label="Download results as CSV",
             data=csv,
-            file_name='resultados_prediccion.csv',
+            file_name='prediction_results.csv',
             mime='text/csv',
         )
     else:
-        st.error("El archivo CSV no contiene las columnas necesarias. Por favor, verifica el formato del archivo.")
+        st.error("The CSV file does not contain the required columns. Please check the file format.")
