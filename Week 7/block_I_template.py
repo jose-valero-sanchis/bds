71e3e3136fb65fe9937cbb7b0447c7f986fca68e
Lucas Fayolle <lucasfayolle@wifialu63-211.wi-fi.upv.es>
2024-10-21 20:14:27 +0200

    Graph

--- Week 7/block_I_template.py
+++ Week 7/block_I_template.py
@@ -25,13 +25,34 @@ plt.grid(True)
 # DISTANCES VECTOR
 points = df.values
 distances = np.sqrt(np.sum((points[:, np.newaxis] - points[np.newaxis, :]) ** 2, axis=-1))
+dist_vector = distances[np.triu_indices(len(points), k=1)] 
 
 # DISTANCES VECTOR EXPLORATION
 # Histogram
 # TODO Draw an histogram to represent the distances, adjusting the number of bins
+# Histogram of distances
+plt.figure(figsize=(8,6))
+plt.hist(dist_vector, bins=30, color='skyblue', edgecolor='black')  # You can adjust 'bins' as needed
+plt.title('Histogram of Pairwise Distances')
+plt.xlabel('Distance')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
 
 # Vector sorting
 # TODO Sort your distances in ascending order
+sorted_distances = np.sort(dist_vector)
+
+plt.figure(figsize=(8,6))
+plt.plot(sorted_distances, marker='o', linestyle='-', color='b')
+plt.title('Sorted Euclidean Distances')
+plt.xlabel('Index')
+plt.ylabel('Distance')
+plt.grid(True)
+plt.show()
+
+
+
 
 # Line plot
 # TODO Draw a line plot to represent the sorted distances
